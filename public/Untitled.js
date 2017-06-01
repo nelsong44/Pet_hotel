4a23881dad9c4dc0ddef95355fb2788085c0f273
Noah Rolf <noahrolf1@gmail.com>
2017-06-01 11:57:30 -0500

    connection with server, sending our table back

--- public/Untitled.js
+++ public/Untitled.js
@@ -1,1 +1,16 @@
+$(document).ready(function(){
+    console.log('JQ');
+    getPets();
 
+});
+
+
+function getPets(){
+  $.ajax({
+    type: 'GET',
+    url: '/pets',
+    success: function( response ){
+      console.log(response);
+    }
+  });
+}
--- server.js
+++ server.js
@@ -2,11 +2,18 @@ var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
-
+var pg = require('pg');
+var config = {
+  database: 'omega',
+  host: 'localhost',
+  port: 5432,
+  max: 12
+};
+var pool = new pg.Pool( config );
 app.use( express.static( 'public' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 
-var searchData = [];
+var petResults = [];
 
 app.listen( 4555, function(){
   console.log('server 4555');
@@ -15,4 +22,26 @@ app.listen( 4555, function(){
 app.get('/', function(req, res){
   console.log('URL hit');
   res.sendFile( path.resolve( 'view/index.html' ) );
+});
+
+app.get('/pets', function(req, res){
+  console.log('URL hit');
+  pool.connect( function( err , connection , done ){
+    if (err){
+      console.log('error in connection', err);
+      done();
+      res.send( 400 );
+    }
+    else {
+      var petData = connection.query('SELECT * FROM petData');
+
+      petData.on( 'row', function(row){
+        petResults.push(row);
+      });
+      petData.on( 'end', function(){
+        done();
+        res.send(petResults)
+      });
+    }
+  });
 });
